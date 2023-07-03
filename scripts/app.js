b0604be4792beef6f9608d90b77663b2d3219f16
negar <negarsaljooghi@gmail.com>
2023-07-03 11:54:58 +0330

    adding icons & img

--- scripts/app.js
+++ scripts/app.js
@@ -1,10 +1,13 @@
 const cityForm = document.querySelector('form');
 const card = document.querySelector('.card');
 const details = document.querySelector('.details');
+const time =document.querySelector('img.time');
+const icon = document.querySelector('.icon img');
 
 const updateUI = () => {
-    const cityDets = data.cityDets;
-    const weather = data.weather;
+
+    //Destructure properties
+    const {cityDets ,weather} = data;
 
     //update details template
     details.innerHTML = `
